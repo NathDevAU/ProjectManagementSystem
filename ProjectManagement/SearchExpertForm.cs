ac6a7fc3229b45883eddbabe7585691d0c555f1a
ivanovg94 <georgipz94@abv.bg>
2017-03-29 20:29:13 +0300

    MEEEEEERGE

---
+++ ProjectManagement/SearchExpertForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManagement
+{
+    public partial class SearchExpertForm : Form
+    {
+        public SearchExpertForm()
+        {
+            InitializeComponent();
+            var experts = new Experts()
+            {
+                FirstName = "Stoyan",
+                MiddleName = "Dimitrov",
+                LastName = "Ruzmanov",
+                IsExternal = false
+            };
+
+            var data = new List<Experts>();
+            this.expertsBindingSource.DataSource = data;
+            //this.ExpertSearchResultGrid.DataBindings.Add("Text", experts.FirstName, "FirstName");
+            //this.ExpertSearchResultGrid.DataBindings.Add(nameof(Experts.MiddleName), experts, nameof(Experts.MiddleName));
+            //this.ExpertSearchResultGrid.DataBindings.Add(nameof(Experts.LastName), experts, nameof(Experts.LastName));
+            //this.ExpertSearchResultGrid.DataBindings.Add(nameof(Experts.IsExternal), experts, nameof(Experts.IsExternal));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Experts
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public bool IsExternal { get; set; }
+    }
+}
